4f7eeed22141c69f4b03544e3bdccc1bd4a45460
hy0817 <72055112+hy0817@users.noreply.github.com>
2020-11-03 16:08:36 +0800

    11/3關卡設計

---
+++ 2DGame/Assets/menu/player.cs
@@ -0,0 +1,14 @@
+﻿
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+    public GameObject final;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        
+        print("碰到傳送門惹ouo");
+        final.SetActive(true);
+    }
+   
+}
