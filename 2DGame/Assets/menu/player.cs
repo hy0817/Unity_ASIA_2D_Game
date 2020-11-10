c768da3f3452ef82181fd1b5e8aac1796331c554
hy0817 <72055112+hy0817@users.noreply.github.com>
2020-11-10 16:05:16 +0800

    1110關卡完成

--- 2DGame/Assets/menu/player.cs
+++ 2DGame/Assets/menu/player.cs
@@ -1,14 +1,26 @@
 ﻿
 using UnityEngine;
+using UnityEngine.UI;
 
 public class player : MonoBehaviour
 {
+
     public GameObject final;
+    public Text textCount;
+    public int count;
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        
-        print("碰到傳送門惹ouo");
-        final.SetActive(true);
+        if (collision.name=="傳送門")
+        {
+            final.SetActive(true);
+        }
+
+        if (collision.tag == "鑽石")
+        {
+            Destroy (collision.gameObject);
+            count++;
+            textCount.text = "Gem:" + count;
+        }
     }
    
 }
